30b4d1fb297228e592b985fcad972b96f1b68009
Samuel Gilbert <samuel.gilbert@canada.ca>
2020-05-25 12:34:51 -0400

    Work In Progress.  Not ready to be used yet!

--- base.cmake
+++
@@ -1,3 +0,0 @@
-cmake_minimum_required(VERSION 3.16)
-
-include("${CMAKE_SOURCE_DIR}/git_version.cmake")
---
+++ compiler_presets.cmake
@@ -0,0 +1,17 @@
+# This modules loads compiler presets for the current platform and handles
+# ECCC's computing environment differently
+
+# Input
+#  COMPILER_SUITE : Lower case name of the compiler suite (gnu, intel, ...)
+
+# Check to see if we are at ECCC
+if(DEFINED ENV{EC_ARCH})
+   message(STATUS "ECCC environment variable detected; ECCC presets")
+   set(COMPILER_PRESET_PATH "ECCC/${BASE_ARCH}/${COMPILER_SUITE}.cmake")
+else()
+   message(STATUS "Using default presets")
+   set(COMPILER_PRESET_PATH "default/${CMAKE_SYSTEM_NAME}-${CMAKE_SYSTEM_PROCESSOR}/${COMPILER_SUITE}.cmake")
+endif()
+
+message(STATUS "Loading preset ${COMPILER_PRESET_PATH}^")
+include("${CMAKE_CURRENT_LIST_DIR}/${COMPILER_PRESET_PATH}")
