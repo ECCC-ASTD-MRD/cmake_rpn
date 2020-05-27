a9bed82d50bcce3075e4d1d878fb1e18866383fa
Samuel Gilbert <samuel.gilbert@canada.ca>
2020-05-27 17:59:36 -0400

    Added an option to build without ECCC's ENV even when it's defined

--- compiler_presets.cmake
+++ compiler_presets.cmake
@@ -5,10 +5,17 @@
 #  COMPILER_SUITE : Lower case name of the compiler suite (gnu, intel, ...)
 #  LANGUAGES : List of language to enable for the project.  Usually C and Fotran
 
-# Check to see if we are at ECCC
-if(DEFINED ENV{EC_ARCH})
-   message(STATUS "ECCC environment variable detected; ECCC presets")
-   set(COMPILER_PRESET_PATH "ECCC/$ENV{BASE_ARCH}/${COMPILER_SUITE}.cmake")
+# TODO: Change this once debugging is done
+option(USE_ECCC_ENV_IF_AVAIL "Use ECCC's custom build envronement" FALSE)
+
+if(USE_ECCC_ENV_IF_AVAIL)
+   if(DEFINED ENV{EC_ARCH})
+      message(STATUS "Using ECCC presets")
+      set(COMPILER_PRESET_PATH "ECCC/$ENV{EC_ARCH}/${COMPILER_SUITE}.cmake")
+   else()
+      message(WARNING "EC_ARCH environment variable not found!  Falling back on default presets")
+      set(COMPILER_PRESET_PATH "default/${CMAKE_SYSTEM_NAME}-${CMAKE_SYSTEM_PROCESSOR}/${COMPILER_SUITE}.cmake")
+   endif()
 else()
    message(STATUS "Using default presets")
    set(COMPILER_PRESET_PATH "default/${CMAKE_SYSTEM_NAME}-${CMAKE_SYSTEM_PROCESSOR}/${COMPILER_SUITE}.cmake")
