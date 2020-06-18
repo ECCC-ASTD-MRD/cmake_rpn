b73f04ad23907bb05422fa94e65fd3eda2f9b1b2
Samuel Gilbert <samuel.gilbert@canada.ca>
2020-06-18 12:19:05 -0400

    Why re-implement features already offered by CMake?

--- doxygen.cmake
+++ doxygen.cmake
@@ -1,16 +1,9 @@
 # Add a target to generate API documentation with Doxygen
 # Taken from https://p5r.uk/blog/2014/cmake-doxygen.html and slightly modified
-find_package(Doxygen)
+
 option(BUILD_DOCUMENTATION "Create and install the HTML based API documentation (requires Doxygen and Graphviz)" ${DOXYGEN_FOUND})
-
 if(BUILD_DOCUMENTATION)
-    if(NOT DOXYGEN_FOUND)
-        message(FATAL_ERROR "Doxygen is needed to build the documentation.")
-    endif()
-
-    if(NOT DOT)
-        message(FATAL_ERROR "Graphviz is needed to build the documentation.")
-    endif()
+    find_package(Doxygen REQUIRED dot)
 
     set(doxyfile_in ${CMAKE_CURRENT_LIST_DIR}/Doxyfile.in)
     set(doxyfile ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
