1fa7437975f0670f4ea3e0fe9b15bb6a25822900
Kristjan Onu <kristjan.onu@canada.ca>
2021-02-12 16:26:05 -0500

    Correct documentation grammar & spelling

--- git_version.cmake
+++ git_version.cmake
@@ -2,7 +2,7 @@
 #  GIT_VERSION : Latest tag obtained or short commit hash obtained from Git
 #  VERSION : The GIT_VERSION if it is compatible with CMake 0.0.0 otherwise
 #
-# This module also add a definition nammed VERSION which contains GIT_VERSION
+# This module also adds a definition named VERSION which contains GIT_VERSION
 
 execute_process(
    COMMAND git describe --tags --always --dirty --broken
