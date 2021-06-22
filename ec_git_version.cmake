ceb89c5c68f2a601fcb810a45e4168b5b1241535
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2021-06-22 18:53:30 +0000

    Missing a few (EC) label in messages.

--- ec_git_version.cmake
+++ ec_git_version.cmake
@@ -15,13 +15,13 @@ macro(ec_git_version)
       ERROR_STRIP_TRAILING_WHITESPACE
    )
    if(${GIT_RESULT} EQUAL 0)
-      message(STATUS "Git version: " ${GIT_VERSION})
+      message(STATUS "(EC) Git version: " ${GIT_VERSION})
       # CMake does not deal with versions that do not follow the form
       # <major>.<minor>.<patch>.<tweak> where each component is a number
       # "0.0.1.0" is valid, but not "0.0.1.fe09182"
       set(VERSION ${GIT_VERSION})
    else()
       set(VERSION "0.0.0")
-      message(FATAL_ERROR "Failed to get version info from Git!\n" "Git error message:\n" ${GIT_ERROR})
+      message(FATAL_ERROR "(EC) Failed to get version info from Git!\n" "Git error message:\n" ${GIT_ERROR})
    endif()
 endmacro()
