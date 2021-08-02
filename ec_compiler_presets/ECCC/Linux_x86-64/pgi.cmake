5d3c14c2f257122b37aed67248def354707fa7d3
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2021-08-02 20:08:03 +0000

    Added links and compiler presets for centos and rhel move flang to llvm

--- ec_compiler_presets/ECCC/Linux_x86-64/pgi.cmake
+++ ec_compiler_presets/ECCC/Linux_x86-64/pgi.cmake
@@ -17,6 +17,3 @@ set(CMAKE_Fortran_FLAGS "-O2 -fpic -byteswapio -fast -Mvect=fuse,simd -Kieee -tr
 set(MPI_Fortran_COMPILE_FLAGS "${MPI_Fortran_COMPILE_FLAGS} ${CMAKE_Fortran_FLAGS}" CACHE STRING "Fortran compiler flags")
 
 set(CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,--allow-shlib-undefined" CACHE STRING "Linker flags" FORCE)
-
-set(LAPACK_LIBRARIES "-llapack")
-set(BLAS_LIBRARIES "-lblas")
