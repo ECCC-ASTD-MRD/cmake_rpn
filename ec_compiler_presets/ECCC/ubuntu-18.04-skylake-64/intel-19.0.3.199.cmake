26b8aea38d49f73d2e5743a820f38e45431af9f9
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2021-08-16 09:45:06 -0400

    Removed warning disable flags

--- ec_compiler_presets/ECCC/ubuntu-18.04-skylake-64/intel-19.0.3.199.cmake
+++ ec_compiler_presets/ECCC/ubuntu-18.04-skylake-64/intel-19.0.3.199.cmake
@@ -25,7 +25,7 @@ set(CMAKE_C_FLAGS "-fp-model precise -traceback -Wtrigraphs" CACHE STRING "C com
 # The impact of -align array32byte is not well known or documented
 set(CMAKE_Fortran_FLAGS_DEBUG "-g -ftrapuv")
 set(CMAKE_Fortran_FLAGS_RELEASE "-O2")
-set(CMAKE_Fortran_FLAGS "-align array32byte -assume byterecl -convert big_endian -fp-model source -fpe0 -traceback -stand f08 -diag-disable 5268 -diag-disable 5140 -diag-disable 7713 -diag-disable 10212" CACHE STRING "Fortran compiler flags" FORCE)
+set(CMAKE_Fortran_FLAGS "-align array32byte -assume byterecl -convert big_endian -fp-model source -fpe0 -traceback -stand f08" CACHE STRING "Fortran compiler flags" FORCE)
 
 set(CMAKE_EXE_LINKER_FLAGS_INIT "--allow-shlib-undefined -static-intel")
 
