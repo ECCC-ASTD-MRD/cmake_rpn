4fbf5657eab39a5cbe86bbb915dab4fe58b786ac
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2022-02-03 14:12:10 +0000

    Added inteloneapo-2022.1.2 preset

---
+++ ec_compiler_presets/ECCC/ubuntu-18.04-skylake-64/inteloneapi-2022.1.2.cmake
@@ -0,0 +1,36 @@
+# Default configuration for the Intel compiler suite
+# Input:
+#  EXTRA_CHECKS Enable extra checking.  This will make the execution slower.
+
+add_definitions(-DLittle_Endian)
+
+set(CMAKE_C_FLAGS "-fp-model precise -traceback -Wtrigraphs" CACHE STRING "C compiler flags" FORCE)
+set(CMAKE_C_FLAGS_DEBUG "-g -ftrapuv")
+set(CMAKE_C_FLAGS_RELEASE "-O2")
+
+# The impact of -align array32byte is not well known or documented
+set(CMAKE_Fortran_FLAGS "-align array32byte -assume byterecl -convert big_endian -fp-model source -fpe0 -traceback -stand f08" CACHE STRING "Fortran compiler flags" FORCE)
+set(CMAKE_Fortran_FLAGS_DEBUG "-g -ftrapuv")
+set(CMAKE_Fortran_FLAGS_RELEASE "-O2")
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT "--allow-shlib-undefined -static-intel")
+
+# There might be extra OpenMP and OpenACC flags which are specific to each compiler,
+# that are not added the find_package(OpenACC)
+# It doesn't matter if we defined them even if OpenACC isn't used because the
+# OpenACC_extra_FLAGS variable just won't be used
+set(OpenACC_extra_FLAGS "-fopt-info-optimized-omp")
+
+# Set the target architecture
+if(NOT TARGET_PROC)
+    set(TARGET_PROC "CORE-AVX512")
+endif()
+message(STATUS "(EC) Target architecture: ${TARGET_PROC}")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -x${TARGET_PROC}")
+set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -x${TARGET_PROC}")
+
+if (EXTRA_CHECKS)
+   set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -warn all -check all")
+   set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -warn all -check all")
+   set(CMAKE_EXE_LINKER_FLAGS_INIT "${CMAKE_EXE_LINKER_FLAGS_INIT} -warn all -check all")
+endif()
