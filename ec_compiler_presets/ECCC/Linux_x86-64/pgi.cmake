e00ef5b0b88403a427a821dcfc3f21b4d8aae719
Samuel Gilbert <samuel.gilbert@canada.ca>
2022-09-16 13:44:10 -0400

    Removed hard coded paths and unneeded -fpic

--- ec_compiler_presets/ECCC/Linux_x86-64/pgi.cmake
+++ ec_compiler_presets/ECCC/Linux_x86-64/pgi.cmake
@@ -6,19 +6,11 @@ add_definitions(-DLittle_Endian)
 
 if("C" IN_LIST languages)
     set(CMAKE_C_FLAGS_RELEASE "-O2" CACHE STRING "C compiler flags" FORCE)
-
-    set(MPI_C_COMPILER "pgcc")
-    set(MPI_C_LIBRARIES  "-L/local/raid/armn/CUDA/PGI/pgi/linux86-64/2017/mpi/openmpi-2.1.2/lib -lmpi")
-    set(MPI_C_INCLUDE_PATH "/local/raid/armn/CUDA/PGI/pgi/linux86-64/2017/mpi/openmpi-2.1.2/include")
 endif()
 
 if("Fortran" IN_LIST languages)
-    set(MPI_Fortran_COMPILER "pgfortran")
-    set(MPI_Fortran_LIBRARIES    "-L/local/raid/armn/CUDA/PGI/pgi/linux86-64/2017/mpi/openmpi-2.1.2/lib -lmpi_mpifh")
-    set(MPI_Fortran_INCLUDE_PATH "/local/raid/armn/CUDA/PGI/pgi/linux86-64/2017/mpi/openmpi-2.1.2/include")
-    set(CMAKE_Fortran_FLAGS "-O2 -fpic -fast -Mvect=fuse,simd -Kieee -traceback" CACHE STRING "Fortran compiler flags" FORCE)
-
-    set(MPI_Fortran_COMPILE_FLAGS "${MPI_Fortran_COMPILE_FLAGS} ${CMAKE_Fortran_FLAGS}" CACHE STRING "Fortran compiler flags")
+    set(CMAKE_Fortran_FLAGS "-fast -Mvect=fuse,simd -Kieee -traceback" CACHE STRING "Fortran compiler flags" FORCE)
+    set(CMAKE_Fortran_FLAGS_RELEASE "-O2")
 endif()
 
 set(CMAKE_EXE_LINKER_FLAGS_INIT "--allow-shlib-undefined" CACHE STRING "Linker flags" FORCE)
