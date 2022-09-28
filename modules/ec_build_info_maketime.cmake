ed76f03c393460ca431e76fd8153149664cce53c
Samuel Gilbert <samuel.gilbert@canada.ca>
2022-09-28 16:01:58 -0400

    Restored setting CMAKE_SOURCE_DIR and CMAKE_CURRENT_SOURCE_DIR

--- modules/ec_build_info_maketime.cmake
+++ modules/ec_build_info_maketime.cmake
@@ -4,9 +4,15 @@
 
 include(${CMAKE_CURRENT_LIST_DIR}/ec_git_version.cmake)
 
+# When a CMake script is executed with "-P", CMAKE_SOURCE_DIR is the current dir
+# Since we don't want that, we force it.  CMAKE_CURRENT_SOURCE_DIR must also be fudged
+# If this isn't done, git won't be able to find the version for out-of-tree builds
+set(CMAKE_SOURCE_DIR "${SOURCE_DIR}")
+set(CMAKE_CURRENT_SOURCE_DIR "${SOURCE_DIR}")
+
 ec_git_version()
 
-set(EC_ARCH   $ENV{EC_ARCH})
-set(EC_USER   $ENV{USER})
+set(EC_ARCH $ENV{EC_ARCH})
+set(EC_USER $ENV{USER})
 string(TIMESTAMP BUILD_TIMESTAMP UTC)
 configure_file(${CMAKE_CURRENT_LIST_DIR}/build_info.h.in ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}_build_info.h @ONLY)
