0e0f0de57be6074b92d9461d9cf588ce3bbcb576
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2023-01-26 14:26:27 +0000

    Fixed doxygen error when not available

--- modules/ec_doxygen.cmake
+++ modules/ec_doxygen.cmake
@@ -4,24 +4,26 @@
 
 # Define doc target only once (in case of projects cascades)
 if (EC_INIT_DONE LESS 2)
-    find_package(Doxygen REQUIRED dot)
+    find_package(Doxygen COMPONENTS dot)
 
-    if(NOT PROJECT_SOURCE_DIR)
-        message(FATAL_ERROR "(EC) PROJECT_SOURCE_DIR not defined!  This module must be called AFTER the project command!")
+    if (Doxygen_FOUND)
+        if(NOT PROJECT_SOURCE_DIR)
+            message(FATAL_ERROR "(EC) PROJECT_SOURCE_DIR not defined!  This module must be called AFTER the project command!")
+        endif()
+
+        set(doxyfile_in ${CMAKE_CURRENT_LIST_DIR}/Doxyfile.in)
+        set(doxyfile ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
+
+        configure_file(${doxyfile_in} ${doxyfile} @ONLY)
+
+        add_custom_target(
+            doc
+            COMMAND ${DOXYGEN_EXECUTABLE} ${doxyfile}
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+            COMMENT "(EC) Generating API documentation with Doxygen"
+            VERBATIM
+        )
+
+        install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/doc/html DESTINATION share/doc/${PROJECT_NAME} OPTIONAL)
     endif()
-
-    set(doxyfile_in ${CMAKE_CURRENT_LIST_DIR}/Doxyfile.in)
-    set(doxyfile ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
-
-    configure_file(${doxyfile_in} ${doxyfile} @ONLY)
-
-    add_custom_target(
-        doc
-        COMMAND ${DOXYGEN_EXECUTABLE} ${doxyfile}
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-        COMMENT "Generating API documentation with Doxygen"
-        VERBATIM
-    )
-
-    install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/doc/html DESTINATION share/doc/${PROJECT_NAME} OPTIONAL)
 endif()
