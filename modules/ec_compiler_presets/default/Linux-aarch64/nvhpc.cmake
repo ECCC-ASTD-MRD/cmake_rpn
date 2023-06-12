c05bbc63db235b2770ce17a6b68450b31a04a235
Vincent Magnoux <vincent.magnoux@ec.gc.ca>
2023-06-12 15:19:14 +0000

    Added presets for nvhpc and armpl (gnu) on ARM

---
+++ modules/ec_compiler_presets/default/Linux-aarch64/nvhpc.cmake
@@ -0,0 +1,14 @@
+# Copyright 2021, Her Majesty the Queen in right of Canada
+
+add_definitions(-DLittle_Endian)
+
+if("C" IN_LIST languages)
+    set(CMAKE_C_FLAGS_RELEASE "-O2")
+endif()
+
+if("Fortran" IN_LIST languages)
+    set(CMAKE_Fortran_FLAGS "-fast -Mvect=fuse,simd -Kieee -traceback" CACHE STRING "Fortran compiler flags" FORCE)
+    set(CMAKE_Fortran_FLAGS_RELEASE "-O2")
+endif()
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT "--allow-shlib-undefined" CACHE STRING "Linker flags" FORCE)
