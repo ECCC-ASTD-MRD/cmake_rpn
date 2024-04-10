d9c8025a6dd7160fb7710ee31a56bc171670336a
Chantal Pic <chantal.pic@ec.gc.ca>
2024-04-10 12:07:38 -0400

    aocc et nvhpc: ajouter les flags utilisés par Michel Valin

--- modules/ec_compiler_presets/default/Linux-x86_64/aocc.cmake
+++ modules/ec_compiler_presets/default/Linux-x86_64/aocc.cmake
@@ -3,14 +3,14 @@
 add_definitions(-DLittle_Endian)
 
 if("C" IN_LIST languages)
+    set(CMAKE_C_FLAGS "-Wno-static-in-inline -march=native" CACHE STRING "C compiler flags" FORCE)
     set(CMAKE_C_FLAGS_DEBUG "-O0 -g")
     set(CMAKE_C_FLAGS_RELEASE "-O2")
 endif()
 
 if("Fortran" IN_LIST languages)
+    set(CMAKE_Fortran_FLAGS "-Mbyteswapio" CACHE STRING "Fortran compiler flags" FORCE)
     set(CMAKE_Fortran_FLAGS_DEBUG "-O0 -g")
     set(CMAKE_Fortran_FLAGS_RELEASE "-O2")
-    set(MPI_Fortran_COMPILE_FLAGS "${MPI_Fortran_COMPILE_FLAGS} ${CMAKE_Fortran_FLAGS}" CACHE STRING "Fortran compiler flags")
 endif()
 
-set(CMAKE_EXE_LINKER_FLAGS_INIT "--allow-shlib-undefined" CACHE STRING "Linker flags" FORCE)
