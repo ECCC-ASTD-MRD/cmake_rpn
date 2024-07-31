9b19b4a628aa95170140c753cd38054b60da0ca9
Chantal Pic <chantal.pic@ec.gc.ca>
2024-07-31 15:51:23 -0400

    Add llvm cmake file

---
+++ modules/ec_compiler_presets/default/Linux-x86_64/llvm.cmake
@@ -0,0 +1,18 @@
+# Copyright 2024, His Majesty the King in Right of Canada 
+ 
+add_definitions(-DLittle_Endian)
+
+if("C" IN_LIST languages)
+  # original flags in previous version of Linux-x86_64-gfortran
+  set(CMAKE_C_FLAGS "-W" CACHE STRING "C compiler flags" FORCE)
+  set(CMAKE_C_FLAGS_DEBUG "-O0 -g")
+  set(CMAKE_C_FLAGS_RELEASE "-O2")
+endif()
+
+if("Fortran" IN_LIST languages)
+  set(CMAKE_Fortran_FLAGS "-fconvert=big-endian -DFLANG_NEW " CACHE STRING "Fortran compiler flags" FORCE)
+  set(CMAKE_Fortran_FLAGS_DEBUG "-O0 -g")
+  set(CMAKE_Fortran_FLAGS_RELEASE "-O2")
+endif()
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-s")
