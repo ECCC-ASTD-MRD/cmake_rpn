9941df48a94bc26c1786cb6b55a5a5a124a0aa6a
Samuel Gilbert <samuel.gilbert@ec.gc.ca>
2024-08-15 14:08:47 +0000

    Reintroduced option to make a doc optional
    - OFF by default
    - Prevents from searching for tools like doxygen when not building documentation

--- modules/ec_doxygen.cmake
+++ modules/ec_doxygen.cmake
@@ -4,42 +4,45 @@
 
 # Define doc target only once (in case of projects cascades)
 if (EC_INIT_DONE LESS 2)
-    find_package(Doxygen COMPONENTS dot)
+    option(WITH_DOC "Create and install the HTML based API documentation (requires Doxygen and Graphviz)" OFF)
+    if(WITH_DOC)
+        find_package(Doxygen COMPONENTS dot)
 
-    if (Doxygen_FOUND)
-        if(NOT PROJECT_SOURCE_DIR)
-            message(FATAL_ERROR "(EC) PROJECT_SOURCE_DIR not defined!  This module must be called AFTER the project command!")
-        endif()
+        if (Doxygen_FOUND)
+            if(NOT PROJECT_SOURCE_DIR)
+                message(FATAL_ERROR "(EC) PROJECT_SOURCE_DIR not defined!  This module must be called AFTER the project command!")
+            endif()
 
-        set(doxyfile_in ${CMAKE_CURRENT_LIST_DIR}/Doxyfile.in)
-        set(doxyfile ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
+            set(doxyfile_in ${CMAKE_CURRENT_LIST_DIR}/Doxyfile.in)
+            set(doxyfile ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
 
-        if (NOT DOC_CALL_GRAPH)
-            set(DOC_CALL_GRAPH "NO")
-        endif()
-        if (NOT DOC_CALLER_GRAPH)
-            set(DOC_CALLER_GRAPH "NO")
-        endif()
-        if (NOT DOC_EXTRACT_ALL)
-            set(DOC_EXTRACT_ALL "NO")
-        endif()
-        if (NOT DOC_INCLUDE_GRAPH)
-            set(DOC_INCLUDE_GRAPH "NO")
-        endif()
-        if (NOT DOC_INCLUDED_BY_GRAPH)
-            set(DOC_INCLUDED_BY_GRAPH "NO")
-        endif()
+            if (NOT DOC_CALL_GRAPH)
+                set(DOC_CALL_GRAPH "NO")
+            endif()
+            if (NOT DOC_CALLER_GRAPH)
+                set(DOC_CALLER_GRAPH "NO")
+            endif()
+            if (NOT DOC_EXTRACT_ALL)
+                set(DOC_EXTRACT_ALL "NO")
+            endif()
+            if (NOT DOC_INCLUDE_GRAPH)
+                set(DOC_INCLUDE_GRAPH "NO")
+            endif()
+            if (NOT DOC_INCLUDED_BY_GRAPH)
+                set(DOC_INCLUDED_BY_GRAPH "NO")
+            endif()
 
-        configure_file(${doxyfile_in} ${doxyfile} @ONLY)
+            configure_file(${doxyfile_in} ${doxyfile} @ONLY)
 
-        add_custom_target(
-            doc
-            COMMAND ${DOXYGEN_EXECUTABLE} ${doxyfile}
-            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-            COMMENT "(EC) Generating API documentation with Doxygen"
-            VERBATIM
-        )
+            add_custom_target(
+                doc
+                COMMAND ${DOXYGEN_EXECUTABLE} ${doxyfile}
+                WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+                COMMENT "(EC) Generating API documentation with Doxygen"
+                VERBATIM
+            )
 
-        install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/doc/html DESTINATION share/doc/${PROJECT_NAME} OPTIONAL)
+            install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/doc/html DESTINATION share/doc/${PROJECT_NAME} OPTIONAL)
+        endif()
     endif()
 endif()
