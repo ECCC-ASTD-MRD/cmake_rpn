466e1cddc13759525107d1547074f999a23f5bec
Samuel Gilbert <samuel.gilbert@ec.gc.ca>
2026-04-27 20:47:22 +0000

    Favor platform specific compiler presets
    Following the discussion in #26, the order in which compiler presets
    are searched was changed in order to try finding one which is platform
    specific before falling back to non-optimized ones.
    
    Use cmake_path(), which allows normalization, to build paths.

--- modules/ec_compiler_presets.cmake
+++ modules/ec_compiler_presets.cmake
@@ -3,6 +3,8 @@
 # This modules loads compiler presets for the current platform and handles
 # ECCC's computing environment differently
 
+cmake_minimum_required(VERSION 3.20)
+
 # Set compiler presets only once (in case of projects cascades)
 if (EC_INIT_DONE LESS 2)
     # CMAKE_BUILD_TYPE can be one of Debug, Release, RelWithDebInfo, MinSizeRel
@@ -26,22 +28,22 @@ if (EC_INIT_DONE LESS 2)
 
     if(DEFINED ENV{EC_ARCH} AND USE_ECCC_ENV_IF_AVAIL)
         set(found "NOTFOUND")
-        if(${EC_ARCH} MATCHES "/")
-            # Check for optimal preset
-            set(COMPILER_PRESET_PATH "ECCC/$ENV{EC_ARCH}")
-            include("ec_compiler_presets/${COMPILER_PRESET_PATH}" OPTIONAL RESULT_VARIABLE found)
 
-            if(NOT found)
-                # Otherwise default to non optimized
-                message(WARNING "(EC) WARNING: Specific preset $ENV{EC_ARCH} not found, switching to default preset")
-                get_filename_component(compName ${EC_ARCH} NAME)
-                set(COMPILER_PRESET_PATH "ECCC/Linux_x86-64/${compName}")
-                include("ec_compiler_presets/${COMPILER_PRESET_PATH}" OPTIONAL RESULT_VARIABLE found)
-            endif()
+        # We can use "cmake_path(HAS_FILENAME EC_ARCH hasComp)" here since if there is no "/",
+        # the platform is specified in EC_ARCH but hasComp will be true. Furthermore, we can't
+        # store the boolean result of the regex matches so we have this ugly construct
+        set(hasComp FALSE)
+        if(EC_ARCH MATCHES "/.+")
+            set(hasComp TRUE)
         endif()
 
-        if(NOT found)
-            message(WARNING "(EC) WARNING: Compiler preset not found, trying the compiler found by CMake")
+        if(hasComp)
+            cmake_path(GET EC_ARCH FILENAME compName)
+            # Try to load specified preset
+            cmake_path(SET COMPILER_PRESET_PATH NORMALIZE "ECCC/$ENV{EC_ARCH}")
+            include("ec_compiler_presets/${COMPILER_PRESET_PATH}" OPTIONAL RESULT_VARIABLE found)
+        else()
+            message(WARNING "(EC) WARNING: Compiler not specified, trying the compiler found by CMake")
             if("C" IN_LIST languages)
                 string(TOLOWER ${CMAKE_C_COMPILER_ID} compName)
             elseif("Fortran" IN_LIST languages)
@@ -49,26 +51,39 @@ if (EC_INIT_DONE LESS 2)
             else()
                 message(FATAL_ERROR "(EC) C and Fortran aren't enabled for this project; automatic compiler identification failed")
             endif()
-            set(COMPILER_PRESET_PATH "ECCC/$ENV{EC_ARCH}/${compName}-${CMAKE_C_COMPILER_VERSION}")
+            # Try to load a preset for the specified architecture and CMake detected compiler version
+            cmake_path(SET COMPILER_PRESET_PATH NORMALIZE "ECCC/$ENV{EC_ARCH}/${compName}-${CMAKE_C_COMPILER_VERSION}")
             include("ec_compiler_presets/${COMPILER_PRESET_PATH}" OPTIONAL RESULT_VARIABLE found)
         endif()
 
         if(NOT found)
             message(WARNING "(EC) WARNING: Preset for the specific compiler version (${CMAKE_C_COMPILER_VERSION}) not found, trying without version")
-            if(${EC_ARCH} MATCHES "/")
+            if(hasComp)
                 cmake_path(GET EC_ARCH PARENT_PATH platform)
+                # Strip away version, if any
+                string(REGEX REPLACE "-.*" "" compName ${compName})
             else()
                 set(platform "${EC_ARCH}")
             endif()
-            set(COMPILER_PRESET_PATH "ECCC/${platform}/${compName}")
+            cmake_path(SET COMPILER_PRESET_PATH NORMALIZE "ECCC/${platform}/${compName}")
             unset(platform)
             include("ec_compiler_presets/${COMPILER_PRESET_PATH}" OPTIONAL RESULT_VARIABLE found)
         endif()
+        unset(hasComp)
+
+        if(NOT found)
+            # Final fallback to non optimized
+            message(WARNING "(EC) WARNING: Specific preset $ENV{EC_ARCH} not found, falling back to non-optimized preset")
+            cmake_path(SET COMPILER_PRESET_PATH NORMALIZE "ECCC/Linux_x86-64/${compName}")
+            message("COMPILER_PRESET_PATH=${COMPILER_PRESET_PATH}")
+            include("ec_compiler_presets/${COMPILER_PRESET_PATH}" OPTIONAL RESULT_VARIABLE found)
+        endif()
+        unset(compName)
     else()
         if(CMAKE_SYSTEM_NAME MATCHES "Linux" AND CMAKE_SYSTEM_PROCESSOR MATCHES "unknown")
             execute_process(COMMAND ${CMAKE_UNAME} -m OUTPUT_VARIABLE CMAKE_SYSTEM_PROCESSOR_exec)
         endif()
-        set(COMPILER_PRESET_PATH "default/${CMAKE_SYSTEM_NAME}-${CMAKE_SYSTEM_PROCESSOR}/${COMPILER_SUITE}")
+        cmake_path(SET COMPILER_PRESET_PATH NORMALIZE "default/${CMAKE_SYSTEM_NAME}-${CMAKE_SYSTEM_PROCESSOR}/${COMPILER_SUITE}")
         include("ec_compiler_presets/${COMPILER_PRESET_PATH}" OPTIONAL RESULT_VARIABLE found)
     endif()
 
